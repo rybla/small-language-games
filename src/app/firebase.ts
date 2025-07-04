55f56baa5a78c545223b60569fe27b7767cda90a
Henry Blanchette <henry@rybl.net>
2025-07-03 23:54:49 -0400

    [checkpoint]

---
+++ src/app/firebase.ts
@@ -0,0 +1,25 @@
+// "use client";
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: "small-language-games.firebaseapp.com",
+//   projectId: "small-language-games",
+//   storageBucket: "small-language-games.firebasestorage.app",
+//   messagingSenderId: "824331383153",
+//   appId: "1:824331383153:web:613bc74360b731f3f0c6d0",
+//   measurementId: "G-CBPN3K7KHD",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
