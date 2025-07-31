07a2cdb8d196bbd603e0e4bb583d2646de918ca0
Henry Blanchette <henry@rybl.net>
2025-07-31 16:27:30 -0400

    [checkpoint]

--- src/backend/ai.ts
+++ src/backend/ai.ts
@@ -7,7 +7,8 @@ export const model = {
   text_speed: googleAI.model("gemini-2.5-flash-lite-preview-06-17"),
   text_power: googleAI.model("gemini-2.5-pro"),
   text_cheap: googleAI.model("gemini-2.0-flash"),
-  image_power: googleAI.model("imagen-4.0-generate-preview-06-06"),
+  // image_power: googleAI.model("imagen-4.0-generate-preview-06-06"),
+  image_power: googleAI.model("imagen-4.0-fast-generate-preview-06-06"),
   image_cheap: googleAI.model("imagen-3.0-generate-002"),
 };
 
